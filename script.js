4e4ee747c00add64d75835cedcf596e397d9bd46
Dunitrashuk <56090992+Dunitrashuk@users.noreply.github.com>
2021-09-09 00:01:10 +0300

    winner logic, simplified the draw function

--- script.js
+++ script.js
@@ -1,20 +1,88 @@
-let symbol = 'X';
-let subGameBoard = generateGameTable();
-let mainGameBoard = generateGameTable();
+let player = 'X';
+let subGameBoard = generateGameBoard();
+let mainGameBoard = generateGameBoard();
+let mainBoxesMap = {
+    "00" : 0,
+    "01" : 0,
+    "02" : 0,
+    "03" : 0,
+    "04" : 0,
+    "05" : 0,
+    "06" : 0,
+    "07" : 0,
+    "08" : 0
+};
 
-
-function generateGameTable() {
+function generateGameBoard() {
     let gameTable = [];
     for(let i = 0; i < 3; i++)
         gameTable[i] = [];
     return gameTable;
 }
 
-function checkWinner(board) {
+function changePlayer() {
+    player === 'X' ? player = 'O' : player = 'X';
+}
+
+function checkWinner(board, player) {
+    if(board[0][0] !== player && board[0][0] === board[0][1] && board[0][1] === board[0][2])
+        return true;
+
+    if(board[1][0] !== player && board[1][0] === board[1][1] && board[1][1] === board[1][2])
+        return true;
+
+    if(board[2][0] !== player && board[2][0] === board[2][1] && board[2][1] === board[2][2])
+        return true;
+
+    if(board[0][0] !== player && board[0][0] === board[1][0] && board[1][0] === board[2][0])
+        return true;
+
+    if(board[0][1] !== player && board[0][1] === board[1][1] && board[1][1] === board[2][1])
+        return true;
+
+    if(board[0][2] !== player && board[0][2] === board[1][2] && board[1][2] === board[2][2])
+        return true;
+
+    if(board[0][0] !== player && board[0][0] === board[1][1] && board[1][1] === board[2][2])
+        return true;
+
+    return board[2][0] !== player && board[2][0] === board[1][1] && board[1][1] === board[0][2];
 
 }
 
 function draw(id) {
+    drawXO(id);
+    evaluateSubGame(id);
+    changePlayer();
+}
+
+function stalemate(board) {
+    let cnt = 0;
+    for(let i = 0; i < board.length; i++)
+        for(let j = 0; j < board.length; j++){
+            if(board[i][j] === 'X' || board[i][j] === 'O')
+                cnt++;
+        }
+    return cnt === 9;
+}
+
+function evaluateSubGame(id) {
+    subGameBoard[parseInt(id[1])][parseInt(id[2])] = player;
+    let parent = document.getElementById(id).parentNode;
+
+    if(checkWinner(subGameBoard)) {
+        mainGameBoard[parseInt(parent.id[0])][parseInt(parent.id[1])] = player;
+        parent.style.pointerEvents = "none";
+        parent.style.background = "whitesmoke";
+
+    } else if(stalemate(subGameBoard)){
+        mainGameBoard[parseInt(parent.id[0])][parseInt(parent.id[1])] = '=';
+        parent.style.background = "grey";
+        parent.style.pointerEvents = "none";
+    }
+}
+
+function drawXO(id) {
     let div = document.getElementById(id);
 
     if(div.innerHTML !== '') // check if the box is populated
@@ -24,18 +92,16 @@ function draw(id) {
     img.style.height = "40px";
     img.style.width = "40px";
     div.innerHTML = '';
-
-    subGameBoard[parseInt(id[1])][parseInt(id[2])] = symbol;
-
-    if(symbol === 'X') {
-        symbol = 'O';
-        img.src = "images/cross.png";
-    } else {
-        symbol = 'X';
-        img.src = "images/circle.png";
-    }
-
+    (player === 'X') ? img.src = "images/cross.png" : img.src = "images/circle.png";
     div.appendChild(img);
-    console.log(subGameBoard);
 }
+
+function lockSubGame() { //this function is responsible for removing all click events from other boxes
+
+}
+
+
+
+
+
 
